0cc8e1c770ed1b43fc510aaa53366c8c1300bcb3
AliHashemiS <79598781+AliHashemiS@users.noreply.github.com>
2022-05-29 15:38:06 -0600

    created connection with twitter

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'FAgUFrxXQN1kAOUuMnr1rAqN5',
+    consumer_secret: 'fKU2MgIcanX5Grr0OSvRhxKxoMBO6qwJJJahiq4hMAYyqoDMxQ',
+    access_token_key: '1520862634094231552-VNHg4TK2kNsRBemIazWwW28t6rozcU',
+    access_token_secret: 'nXvGl9drqzDiBPBiyohonmNLA45TdPFtMKX3q42ps8sMW'
+});
+
+setInterval(()=>{
+  client.post('statuses/update', {status: '#salvandoRedes2022IC'})
+  .then(function (tweet) {
+    console.log(tweet);
+  })
+  .catch(function (error) {
+    throw error;
+  })
+}, 600000);
+
